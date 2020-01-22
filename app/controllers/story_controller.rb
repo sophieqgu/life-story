8b3b0f9d3c7219001b09717ff1c94a84450753ab
Sophie Gu <sophieqgu@gmail.com>
2020-01-22 21:44:47 +0000

    Fix database error

--- app/controllers/story_controller.rb
+++ app/controllers/story_controller.rb
@@ -8,5 +8,20 @@ class StoryController < ApplicationController
       redirect '/'
     end 
   end 
+  
+  
+  post '/' do 
+    @story = Story.create(summary: params[:summary], date: params[:date], description: params[:description])
+    @story.category = Category.find(params[:category])
+    @story.users << current_user
+    if !params[:users].nil?
+      params[:users].each do |user_id|
+        @story.users << User.find(user_id)
+      end 
+    end 
+    @story.save
+    flash[:success] = "Story published successfully."
+    redirect '/'
+  end 
 
 end 
